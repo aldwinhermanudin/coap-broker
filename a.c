6ec71f2cfcdc822adcca70d7a6f6fdf47a0afc4a
adikabintang <adikabintang78@gmail.com>
2016-03-16 07:40:51 +0700

    fcgi2

--- a.c
+++ a.c
@@ -1,16 +1,20 @@
 #include <fcgiapp.h>
+#include <string.h>
 
 int main() {
-  FCGX_Stream *in, *out, *err;
-  FCGX_ParamArray envp;
-  while (FCGX_Accept(&in, &out, &err, &envp) >= 0) {
-    char *q = FCGX_GetParam("QUERY_STRING", envp);
-    FCGX_FPrintF(out, "Content-type: text/plain\r\n\r\n");
-    if (!q) {
-      FCGX_FPrintF(out,
-          "no query string: check web server configuration\n");
-    }
-    FCGX_FPrintF(out, "Query: '%s'\n", q);
-  }
-  return 0;
+	FCGX_Stream *in, *out, *err;
+	FCGX_ParamArray envp;
+	while (FCGX_Accept(&in, &out, &err, &envp) >= 0) {
+		char *q = FCGX_GetParam("QUERY_STRING", envp);
+		FCGX_FPrintF(out, "Content-type: text/plain\r\n\r\n");
+		if (!q) {
+			FCGX_FPrintF(out,
+				"no query string: check web server configuration\n");
+		}
+		FCGX_FPrintF(out, "Query: '%s'\n", q);
+		if (strcmp(q, "kancut")) {
+			FCGX_FPrintF(out, "Belel\n");		
+		}
+	}
+	return 0;
 }
