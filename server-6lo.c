27ea82b4f967855721a3eee4b6b1e0aa05cb89b1
adikabintang <adikabintang78@gmail.com>
2016-03-25 14:11:18 +0700

    coba lagi

--- server-6lo.c
+++ server-6lo.c
@@ -7,7 +7,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include "../src/nl802154.h"
+# include "ieee802154.h"
 //#include <net/af_ieee802154.h>
 
 void error(const char *msg)
@@ -21,11 +21,11 @@ int main(int argc, char *argv[])
      int sockfd, newsockfd, portno;
      socklen_t clilen;
      char buffer[256];
-     struct sockaddr_ieee802154 serv_addr, cli_addr;
+     //struct sockaddr_ieee802154 serv_addr, cli_addr;
      int n;
      
      char *some_addr;
-     char str[IEEE802154_ADDR_LEN];
+     //char str[IEEE802154_ADDR_LEN];
      /*if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
