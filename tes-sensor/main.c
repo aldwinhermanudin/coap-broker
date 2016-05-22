b1a01a380d426e77e6ff7886f66102b9b091e1e9
adikabintang <adikabintang78@gmail.com>
2016-05-23 05:56:50 +0700

    f

--- tes-sensor/main.c
+++ tes-sensor/main.c
@@ -3,40 +3,30 @@
 #include <unistd.h>
 #include "sensorNode.h"
 
-
+/*
 int main(int argc, char *argv[])
 {
   int fd, n, i;
   char buf[64] = "temp text";
   struct termios toptions;
 
-  /* open serial port */
-  fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
+  fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
   printf("fd opened as %i\n", fd);
   
-  /* wait for the Arduino to reboot */
   usleep(3500000);
   
-  /* get current serial port settings */
   tcgetattr(fd, &toptions);
-  /* set 9600 baud both ways */
   cfsetispeed(&toptions, B9600);
   cfsetospeed(&toptions, B9600);
-  /* 8 bits, no parity, no stop bits */
   toptions.c_cflag &= ~PARENB;
   toptions.c_cflag &= ~CSTOPB;
   toptions.c_cflag &= ~CSIZE;
   toptions.c_cflag |= CS8;
-  /* Canonical mode */
   toptions.c_lflag |= ICANON;
-  /* commit the serial port settings */
   tcsetattr(fd, TCSANOW, &toptions);
 
-  /* Send byte to trigger Arduino to send string back */
   write(fd, "0", 1);
-  /* Receive string from Arduino */
   n = read(fd, buf, 64);
-  /* insert terminating zero in the string */
   buf[n] = 0;
 
   printf("%i bytes read, buffer contains: %s\n", n, buf);
@@ -53,10 +43,10 @@ int main(int argc, char *argv[])
 
   return 0;
 }
+*/
 
 
 
-/*
 int main() {
 	int i, x;
 	initSerial("/dev/ttyUSB0", B9600);
@@ -74,4 +64,4 @@ int main() {
 	sleep(2);
 	return 0;
 }
-*/
+
