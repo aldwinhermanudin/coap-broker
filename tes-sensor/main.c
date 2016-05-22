8a962bfd164b4f67fe2803a88eb0b3f6f49df383
adikabintang <adikabintang78@gmail.com>
2016-05-23 05:38:10 +0700

    s

---
+++ tes-sensor/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "sensorNode.h"
+
+int main() {
+	int i, x;
+	
+	printf("1. tes ADC...\n");
+	for (i = 0; i < 6; i++) {
+		x = getADC(0);
+		printf("%d - ADC: %d\n", i, x);
+	}
+	printf("tes GPIO...\n");
+	printf("matiin...\n");
+	turnGPIO(3, 0);
+	sleep(4);
+	printf("nyalain...\n");
+	turnGPIO(3, 1);
+	sleep(2);
+	return 0;
+}
