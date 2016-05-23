e445bfb07eb7a8a288190c9e2bdc49b1fd501cc8
adikabintang <adikabintang78@gmail.com>
2016-05-23 07:10:16 +0700

    f

--- tes-sensor/main.c
+++ tes-sensor/main.c
@@ -58,16 +58,16 @@ int main() {
 	initSerial("/dev/ttyUSB0", B9600);
 	printf("1. tes ADC...\n");
 	//for (i = 0; i < 6; i++) {
-		x = getADC(0);
-		printf("%d - ADC: %d\n", i, x);
+	//	x = getADC(0);
+	//	printf("%d - ADC: %d\n", i, x);
 	//}
 	printf("tes GPIO...\n");
 	printf("matiin...\n");
 	turnGPIO(3, 0);
 	sleep(4);
-	printf("nyalain...\n");
-	turnGPIO(3, 1);
-	sleep(2);
+	//printf("nyalain...\n");
+	//turnGPIO(3, 1);
+	//sleep(2);
 	return 0;
 }
 
